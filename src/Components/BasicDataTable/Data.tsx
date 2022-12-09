eddbb122716a7f989273ea9ea022de56a45ff398
Deepak Singh <deepak.singh@growthnatives.co>
2022-12-09 13:33:51 +0530

    adding sorting in data table

--- src/Components/BasicDataTable/Data.tsx
+++ src/Components/BasicDataTable/Data.tsx
@@ -2,18 +2,22 @@ export const columns = [
   {
     name: 'Id',
     selector: (row:any) => row.id,
+    sortable: true,
 },
   {
       name: 'Title',
       selector: (row:any) => row.title,
+      sortable: true,
   },
   {
       name: 'Year',
       selector: (row:any) => row.year,
+      sortable: true,
   },
   {
     name: 'Action',
     selector: (row:any) => <a href="#">edit</a>,
+    sortable: true
 },
 ];
 
